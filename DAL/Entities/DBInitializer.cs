16159b598e5548426270877c251893696b91e91d
Zergond-ПК\Zergond <azimut965@gmail.com>
2017-11-05 16:54:56 +0200

    DAL is good

---
+++ DAL/Entities/DBInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace DAL.Entities
+{
+    internal class DBInitializer : CreateDatabaseIfNotExists<AppDBContext>
+    {
+        protected override void Seed(AppDBContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
---
+++ DAL/Entities/Identity/AppUser.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities.Identity
+{
+    class AppUser:IdentityUser
+    {
+        [Required]
+        [MaxLength(20, ErrorMessage = "Name can't be longer than 20 characters")]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(20, ErrorMessage = "Last name can't be longer than 20 characters")]
+        public string LastName { get; set; }
+        [Required]
+        [Range(0, 120, ErrorMessage = "Age has to be between 0 and 120")]
+        public int Age { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime RegisteredDate { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime StudyDate { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<AppUser> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+    }
+}
