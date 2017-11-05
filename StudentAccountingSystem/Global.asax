04cdca192e49c5804949e9e4d8291caf520cf723
Zergond-ПК\Zergond <azimut965@gmail.com>
2017-11-05 15:29:31 +0200

    Add project files.

---
+++ StudentAccountingSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentAccountingSystem.MvcApplication" Language="C#" %>
---
+++ StudentAccountingSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentAccountingSystem.Startup))]
+namespace StudentAccountingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
